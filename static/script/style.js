a58b295991000b7ef857652255283f9a92bedbfb
Spremic <dspremic1@gmail.com>
2023-01-22 20:43:51 +0100

    commit 22.01.23

--- static/script/style.js
+++ static/script/style.js
@@ -1,9 +1,7 @@
 let openHamburger = document.querySelector("#hamburger");
 let hamburgerData = document.querySelector(".desingNavigation");
 let putanjaOnama = document.querySelector(".oNamaSekcija");
-putanjaOnama.addEventListener("click", () => {
-  location.href = "/static/about.html";
-});
+
 openHamburger.addEventListener("click", () => {
   if (hamburgerData.style.display === "block") {
     hamburgerData.style.display = "none";
@@ -12,3 +10,8 @@ openHamburger.addEventListener("click", () => {
     hamburgerData.style.margin = "0px 0px 120px 0px";
   }
 });
+if (putanjaOnama) {
+  putanjaOnama.addEventListener("click", () => {
+    location.href = "/static/about.html";
+  });
+}
